7686bab188ca1716ba7aa70848df5bf987bc20bd
Bolo Michelin <bolo@lordalexworks.com>
2012-10-01 01:10:17 -0400

    initial commit-

---
+++ js/application.js
@@ -0,0 +1,21 @@
+$(function() {   		
+	// email
+			
+	$.fn.EmailConverter=function()
+	{
+		$(this).each(function() 
+		{
+			var title = $(this).attr('title');
+			var link = title;
+			link = link.replace(/ dot /gi, '.');
+			link = link.replace(/ at /gi, '@' );
+			if(title == $(this).html())
+			{
+				$(this).html(link);
+			}
+			$(this).attr('title', 'Get in touch');
+			$(this).attr('href', "mailto:"+link);
+		});
+	}
+	$('.obfs-email').EmailConverter();
+});
